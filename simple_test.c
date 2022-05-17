ecaffd34848e5f37295f7d0a5fb2f0689bf4800e
jniemine <jniemine@student.hive.fi>
2022-05-17 18:08:22 +0300

    Test at school

--- simple_test.c
+++ simple_test.c
@@ -773,39 +773,10 @@ void test_unsigned()
 
 void where_is_leak()
 {
-//  ft_printf("\\n");
-//  ft_printf("%%\\n");
-//  ft_printf("%d\\n", 42);
-//  ft_printf("%d%d\\n", 42, 41);
-//  ft_printf("%d%d%d\\n", 42, 43, 44);
-//  ft_printf("%ld\\n", 2147483647);
-//  ft_printf("%lld\\n", 9223372036854775807);
-//  ft_printf("%x\\n", 505);
-//  ft_printf("%X\\n", 505);
-//  ft_printf("%p\\n", &ft_printf);
-//  ft_printf("%20.15d\\n", 54321);
-//  ft_printf("%-10d\\n", 3);
-//  ft_printf("% d\\n", 3);
-//  ft_printf("%+d\\n", 3);
-//  ft_printf("%010d\\n", 1);
-//  ft_printf("%hhd\\n", 0);
-//  ft_printf("%jd\\n", 9223372036854775807);
-
-//  ft_printf("%zd\\n", 4294967295);
-//  ft_printf("%\\n");
-  	ft_printf("%U", 4294967295);
-
-//  ft_printf("%u\\n", 4294967295);
-//  ft_printf("%o\\n", 40);
-//  ft_printf("%%#08x\\n", 42);
-//  ft_printf("%x\\n", 1000);
-
-//  ft_printf("%#X\\n", 1000);
-//  ft_printf("%s\\n", NULL);
-//  ft_printf("%S\\n", L"ݗݜशব");
-//  ft_printf("%s%s\\n", "test", "test");
-//  ft_printf("%s%s%s\\n", "test", "test", "test");
-//  ft_printf("%C\\n", 15000);
+	int res1, res2;
+	res1 = printf("|%*d|\n", INT_MAX, 4);
+	res2 = ft_printf("|%*d|\n", INT_MAX, 4);
+	printf("res1: %d res2: %d\n", res1, res2);
 }
 /* Anything excpet float with value 0 and precisionn zero prints no digits */
 int main(void)
@@ -819,7 +790,7 @@ int main(void)
 //	test_d_i();
 //	test_x();
 //	wtf();
-	test_octal_len();
+//	test_octal_len();
 //	test_print_octal();
 /*	copy tests from percent and interesting to automatic test file NOW */
 //	test_percent();
@@ -833,6 +804,6 @@ int main(void)
 //	test_ptr();
 //	test_minus();
 //	test_unsigned();
-//	where_is_leak();
+	where_is_leak();
 	return (0);
 }
